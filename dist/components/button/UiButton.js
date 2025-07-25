5d6d479bbe631a5736b7bc0d73fe4b964f9b75f4
ikhartov <i.khartov@upstars.com>
2025-07-25 12:41:28 +0300

    chore: cleanup dist components and unused files

--- dist/components/button/UiButton.js
+++
@@ -1,4 +0,0 @@
-import { _ as f } from "../../lib/UiButton.chunk.js";
-export {
-  f as default
-};
--- dist/components/index.js
+++
@@ -1,9 +0,0 @@
-import { _ as t } from "../lib/UiButton.chunk.js";
-import { _ as d } from "../lib/UiIcon.chunk.js";
-import { addIcon as c, addIcons as e } from "./icon/icons.js";
-export {
-  t as UiButton,
-  d as UiIcon,
-  c as addIcon,
-  e as addIcons
-};
