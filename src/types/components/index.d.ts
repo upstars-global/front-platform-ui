a0bab3a62f5677140ed52b19109d2e4612e5a850
ikhartov <i.khartov@upstars.com>
2025-09-17 20:22:26 +0300

    feat: export `UiKitConfig` type from components index file

--- src/types/components/index.d.ts
+++ src/types/components/index.d.ts
@@ -4,4 +4,5 @@ import UiLink from './link/UiLink.vue';
 export type { UiIconProps } from './icon/UiIcon.vue';
 export type { UiLinkProps, UiLinkEmits } from './link/UiLink.vue';
 export type { UiIconsFill, UiIconName, UiIconsNavigation, UiIconsOutline, UiIconsSocial } from './icon/config';
+export type { AppConfig as UiKitConfig } from './types';
 export { UiCircleProgressBar, UiIcon, UiLink };
