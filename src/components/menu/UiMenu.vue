f5c994a42d8b2f279085ed572e63bc20eca676bd
ikhartov <i.khartov@upstars.com>
2025-11-11 17:27:34 +0200

    feat(FP-3533): add `UiMenu` component with theme support and Storybook integration

---
+++ src/components/menu/UiMenu.vue
@@ -0,0 +1,99 @@
+<script lang="ts">
+import type { RouteLocationRaw } from 'vue-router'
+import type { ClassNameValue } from 'tailwind-merge'
+import type { UiIconName } from '../icon/config'
+import type { UiProp } from '../types'
+import type { MenuUi } from './theme'
+
+export interface UiMenuItem {
+  routerName?: string
+  name?: string
+  iconName?: UiIconName
+  isHidden?: boolean
+  to?: RouteLocationRaw | string
+  dataTest?: string
+}
+
+export interface UiMenuProps {
+  items: UiMenuItem[]
+  ui?: UiProp<MenuUi>
+}
+</script>
+<script setup lang="ts">
+import { computed } from 'vue'
+import { useRouter } from 'vue-router'
+import { useAppConfig } from '../../composables/useAppConfig'
+import { useComponentAttributes } from '../../composables/useUiClasses'
+import theme from './theme'
+import UiIcon from '../icon/UiIcon.vue'
+import UiLink from '../link/UiLink.vue'
+
+defineOptions({
+  name: 'UiMenu',
+  inheritAttrs: false
+})
+
+const props = withDefaults(defineProps<UiMenuProps>(), {
+  ui: () => ({ strategy: 'merge' })
+})
+
+const router = useRouter()
+
+const appConfig = useAppConfig()
+const { attributes, className, mergeClasses } = useComponentAttributes(
+  'ui-menu',
+  computed(() => {
+    const commonClasses: ClassNameValue[] = [theme.base, appConfig?.ui?.menu?.base, props.ui?.base].filter(Boolean)
+
+    return commonClasses
+  }),
+  appConfig?.ui?.menu?.strategy || props.ui?.strategy
+)
+
+const uiClasses = computed(() => {
+  return {
+    item: {
+      base: mergeClasses(theme.item.base, appConfig?.ui?.menu?.item?.base, props.ui?.item?.base),
+      active: mergeClasses(theme.item.active, appConfig?.ui?.menu?.item?.active, props.ui?.item?.active)
+    }
+  }
+})
+
+const getPath = (path?: RouteLocationRaw | string) => {
+  if (!path) {
+    return ''
+  }
+  if (typeof path === 'object') {
+    return router.resolve(path).path
+  }
+  return path.startsWith('/') ? path : `/${path}`
+}
+const getItemPath = (item: UiMenuItem): RouteLocationRaw | string => {
+  if (item.routerName) {
+    return item.routerName.startsWith('#') ? { path: item.routerName } : { name: item.routerName }
+  }
+
+  if (item.to) {
+    return getPath(item.to)
+  }
+
+  return ''
+}
+</script>
+
+<template>
+  <div v-bind="attributes" :class="className">
+    <template v-for="(item, index) in items" :key="index">
+      <UiLink
+        v-if="!item.isHidden"
+        :to="getItemPath(item)"
+        :data-test="item.dataTest"
+        :class="uiClasses.item.base"
+        :active-class="uiClasses.item.active"
+      >
+        <UiIcon v-if="item.iconName" :name="item.iconName" class="mr-2" />
+        {{ item.name }}
+      </UiLink>
+    </template>
+  </div>
+</template>
