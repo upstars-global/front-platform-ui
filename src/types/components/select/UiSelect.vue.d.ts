ce72660bcad2d3631af26018f1277e434cdaf62b
d-balianytsia <d-balianytsia@users.noreply.github.com>
2025-11-27 08:57:08 +0000

    build: generate type definitions

--- src/types/components/select/UiSelect.vue.d.ts
+++ src/types/components/select/UiSelect.vue.d.ts
@@ -1,6 +1,6 @@
 import type { FormElementProps, UiProp } from '../types';
 import type { SelectUi } from './theme';
-import type { UiTooltipProps } from '../../types/components/tooltip/UiTooltip.vue';
+import { type UiTooltipProps } from '../tooltip/UiTooltip.vue';
 export interface SelectOption {
     label: string;
     value: string | number;
@@ -32,29 +32,29 @@ interface UiSelectSlots {
 }
 type __VLS_Slots = UiSelectSlots;
 declare const __VLS_base: import("vue").DefineComponent<UiSelectProps, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {} & {
-    "update:modelValue": (value: string | number) => any;
     change: (value: string | number) => any;
-    focus: (value: FocusEvent) => any;
     blur: (value: FocusEvent) => any;
+    focus: (value: FocusEvent) => any;
+    "update:modelValue": (value: string | number) => any;
 }, string, import("vue").PublicProps, Readonly<UiSelectProps> & Readonly<{
-    "onUpdate:modelValue"?: ((value: string | number) => any) | undefined;
     onChange?: ((value: string | number) => any) | undefined;
-    onFocus?: ((value: FocusEvent) => any) | undefined;
     onBlur?: ((value: FocusEvent) => any) | undefined;
+    onFocus?: ((value: FocusEvent) => any) | undefined;
+    "onUpdate:modelValue"?: ((value: string | number) => any) | undefined;
 }>, {
-    offsetValue: number;
+    error: string;
+    id: string;
+    options: SelectOption[];
+    size: "small" | "medium";
+    description: string;
+    placeholder: string;
+    ui: UiProp<SelectUi>;
+    fullWidth: boolean;
     dataTest: string;
     modelValue: string | number;
-    error: string;
-    description: string;
+    offsetValue: number;
     subLabel: string;
-    ui: UiProp<SelectUi>;
     selectTextAlign: "left" | "center" | "right";
-    options: SelectOption[];
-    placeholder: string;
-    id: string;
-    size: "small" | "medium";
-    fullWidth: boolean;
 }, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>;
 declare const __VLS_export: __VLS_WithSlots<typeof __VLS_base, __VLS_Slots>;
 declare const _default: typeof __VLS_export;
