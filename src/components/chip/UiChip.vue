bb423d82d44eb31633b7d4936fb36c8c6b07c20a
ikhartov <i.khartov@upstars.com>
2026-02-24 16:47:35 +0200

    feat(chip): add support for leading and trailing icons for UiChip

--- src/components/chip/UiChip.vue
+++ src/components/chip/UiChip.vue
@@ -2,6 +2,8 @@
 import type { ClassNameValue } from 'tailwind-merge'
 import type { UiProp } from '../types'
 import type { ChipUi } from './theme'
+import type { UiIconName } from '../icon/config'
+import type { IconSize } from '../icon/UiIcon.vue'
 
 export interface UiChipProps<T = string | number> {
   label: string
@@ -11,14 +13,21 @@ export interface UiChipProps<T = string | number> {
   isSpecial?: boolean
   isActive?: boolean
   count?: number
+  leadingIconName?: UiIconName
+  leadingIconSize?: IconSize
+  trailingIconName?: UiIconName
+  trailingIconSize?: IconSize
   ui?: UiProp<ChipUi>
 }
 
 export interface UiChipEmits {
   (event: 'click', value: MouseEvent): void
+  (event: 'click-leading', value: MouseEvent): void
+  (event: 'click-trailing', value: MouseEvent): void
 }
 
 export interface UiChipSlots {
+  leading(): unknown
   trailing(): unknown
 }
 </script>
@@ -28,6 +37,7 @@ import { computed } from 'vue'
 import { useAppConfig } from '../../composables/useAppConfig'
 import { useComponentAttributes } from '../../composables/useUiClasses'
 import { prepareVariants } from '../../helpers/prepareClassNames'
+import UiIcon from '../icon/UiIcon.vue'
 import theme from './theme'
 
 defineOptions({
@@ -38,13 +48,17 @@ defineOptions({
 const props = withDefaults(defineProps<UiChipProps>(), {
   dataTest: undefined,
   count: undefined,
+  leadingIconName: undefined,
+  leadingIconSize: '16',
+  trailingIconName: undefined,
+  trailingIconSize: '16',
   ui: undefined
 })
 const emit = defineEmits<UiChipEmits>()
 defineSlots<UiChipSlots>()
 
 const appConfig = useAppConfig()
-const { attributes, className } = useComponentAttributes(
+const { attributes, className, mergeClasses } = useComponentAttributes(
   'ui-chip',
   computed(() => {
     const commonClasses: ClassNameValue[] = [theme.base, appConfig.ui?.chip?.base, props.ui?.base].filter(Boolean)
@@ -67,11 +81,35 @@ const { attributes, className } = useComponentAttributes(
   }),
   appConfig.ui?.chip?.strategy || props.ui?.strategy
 )
+
+const uiClasses = computed(() => {
+  return {
+    leadingIcon: mergeClasses(theme.leadingIcon, appConfig.ui?.chip?.leadingIcon, props.ui?.leadingIcon),
+    trailingIcon: mergeClasses(theme.trailingIcon, appConfig.ui?.chip?.trailingIcon, props.ui?.trailingIcon)
+  }
+})
 </script>
 
 <template>
   <div :class="className" :data-test="dataTest" v-bind="attributes" @click="emit('click', $event)">
+    <slot name="leading">
+      <UiIcon
+        v-if="leadingIconName"
+        :name="leadingIconName"
+        :size="leadingIconSize"
+        :class="uiClasses.leadingIcon"
+        @click.stop="emit('click-leading', $event)"
+      />
+    </slot>
     {{ label }}
-    <slot name="trailing" />
+    <slot name="trailing">
+      <UiIcon
+        v-if="trailingIconName"
+        :name="trailingIconName"
+        :size="trailingIconSize"
+        :class="uiClasses.trailingIcon"
+        @click.stop="emit('click-trailing', $event)"
+      />
+    </slot>
   </div>
 </template>
--- src/types/components/chip/UiChip.vue.d.ts
+++ src/types/components/chip/UiChip.vue.d.ts
@@ -1,5 +1,7 @@
 import type { UiProp } from '../types';
 import type { ChipUi } from './theme';
+import type { UiIconName } from '../icon/config';
+import type { IconSize } from '../icon/UiIcon.vue';
 export interface UiChipProps<T = string | number> {
     label: string;
     name: T;
@@ -8,23 +10,38 @@ export interface UiChipProps<T = string | number> {
     isSpecial?: boolean;
     isActive?: boolean;
     count?: number;
+    leadingIconName?: UiIconName;
+    leadingIconSize?: IconSize;
+    trailingIconName?: UiIconName;
+    trailingIconSize?: IconSize;
     ui?: UiProp<ChipUi>;
 }
 export interface UiChipEmits {
     (event: 'click', value: MouseEvent): void;
+    (event: 'click-leading', value: MouseEvent): void;
+    (event: 'click-trailing', value: MouseEvent): void;
 }
 export interface UiChipSlots {
+    leading(): unknown;
     trailing(): unknown;
 }
 declare const _default: typeof __VLS_export;
 export default _default;
 declare const __VLS_export: __VLS_WithSlots<import("vue").DefineComponent<UiChipProps<string | number>, {}, {}, {}, {}, import("vue").ComponentOptionsMixin, import("vue").ComponentOptionsMixin, {} & {
     click: (value: MouseEvent) => any;
+    "click-leading": (value: MouseEvent) => any;
+    "click-trailing": (value: MouseEvent) => any;
 }, string, import("vue").PublicProps, Readonly<UiChipProps<string | number>> & Readonly<{
     onClick?: ((value: MouseEvent) => any) | undefined;
+    "onClick-leading"?: ((value: MouseEvent) => any) | undefined;
+    "onClick-trailing"?: ((value: MouseEvent) => any) | undefined;
 }>, {
     ui: UiProp<ChipUi>;
+    trailingIconName: UiIconName;
+    trailingIconSize: IconSize;
     dataTest: string;
+    leadingIconName: UiIconName;
+    leadingIconSize: IconSize;
     count: number;
 }, {}, {}, {}, string, import("vue").ComponentProvideOptions, false, {}, any>, UiChipSlots>;
 type __VLS_WithSlots<T, S> = T & {
