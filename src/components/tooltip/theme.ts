a6d9ff3fb254c749b737ee50507eeff4f9438a1b
ikhartov <i.khartov@upstars.com>
2026-03-17 16:56:55 +0200

    fix(tooltip): update transition duration and easing for tooltip body

--- src/components/tooltip/theme.ts
+++ src/components/tooltip/theme.ts
@@ -1,7 +1,7 @@
 const tooltip = {
   base: 'relative align-middle',
   activator: 'align-middle cursor-pointer',
-  body: 'box-border text-center text-white text-xs absolute z-10 transition-opacity duration-200 ease-in-out invisible opacity-0',
+  body: 'box-border text-center text-white text-xs absolute z-10 transition duration-500 ease-easing invisible opacity-0',
   bodyVisible: '!visible !opacity-100',
   arrow: 'absolute z-10 block w-2 h-2 rotate-45'
 }
