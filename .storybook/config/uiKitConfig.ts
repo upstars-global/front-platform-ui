b66e2b2a3b94b9fc920cd65df1c313adc88a38fd
ikhartov <i.khartov@upstars.com>
2026-04-22 15:07:39 +0300

    feat(button): adjust primary and ghost variants, refine active styles

--- .storybook/config/uiKitConfig.ts
+++ .storybook/config/uiKitConfig.ts
@@ -91,7 +91,7 @@ export const uiKitConfig: AppConfig = {
         primary: 'bg-primary-20 hover:bg-primary-30 active:bg-primary-50',
         secondary: 'bg-secondary-30 hover:bg-secondary-20 active:bg-secondary-40',
         tertiary: 'shadow-tertiary-40 text-tertiary-40 hover:text-light hover:bg-tertiary-20',
-        ghost: 'text-dark hover:text-light hover:bg-tertiary-20 active:bg-tertiary-30'
+        ghost: '!text-support hover:!text-light hover:bg-tertiary-20 active:bg-tertiary-30'
       },
       active: {
         primary: '!bg-primary-50',
